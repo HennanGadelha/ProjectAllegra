92a1f6b13f9fa4a6f11719dd15884c3fac3b118c
Hennan Gadelha <43623515+HennanGadelha@users.noreply.github.com>
2019-05-11 23:35:00 -0300

    implemetando classe de vendas

--- models/Sale.php
+++ models/Sale.php
@@ -1,21 +1,39 @@
 <?php
+require('core/Database.php');
 
 class Sale{
 
-    private $codSale;
-    private $value;
+    
     private $date;
+    private $connection;
     
 
     
 
-    public function __construct($codSale, $value, $date){
+    public function __construct(){
         
-        $this->codSale = ;
-        $this->value = $value;
-        $this->date = $date;
-
+        $this->connection = (new Database())->connect();
+        $this->date = date("d-m-Y H:i:s");
 
     }
+
+
+    public function insert($value) {
+
+        $sql = 'INSERT INTO sale ';
+        $sql .= '(value, date) ';
+        $sql .= 'VALUES (:value, now())';
+
+        $sale = $this->connection->prepare($sql);
+
+        $sale->bindValue(':value', $value, PDO::PARAM_INT);
+        $sale->bindValue('now', $data['value'], PDO::PARAM_INT);
+         
+         
+        $sale->execute();
+
+        return $this->connection->lastInsertId();
+
+    } 
 
 }
